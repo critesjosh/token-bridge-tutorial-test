50fb284648ace2fb34a38a349d3909f4b29799aa
Josh Crites <jc@joshcrites.com>
2024-06-18 12:11:36 -0400

    working with 0.43

--- packages/src/test/fixtures/TokenBridge.ts
+++ packages/src/test/fixtures/TokenBridge.ts
@@ -118,31 +118,31 @@ portal_address: {
   /** Type-safe wrappers for the public methods exposed by the contract. */
   public override methods!: {
     
-    /** get_portal_address() */
-    get_portal_address: (() => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+    /** compute_note_hash_and_optionally_a_nullifier(contract_address: struct, nonce: field, storage_slot: field, note_type_id: field, compute_nullifier: boolean, serialized_note: array) */
+    compute_note_hash_and_optionally_a_nullifier: ((contract_address: AztecAddressLike, nonce: FieldLike, storage_slot: FieldLike, note_type_id: FieldLike, compute_nullifier: boolean, serialized_note: FieldLike[]) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
     /** exit_to_l1_public(recipient: struct, amount: field, caller_on_l1: struct, nonce: field) */
     exit_to_l1_public: ((recipient: EthAddressLike, amount: FieldLike, caller_on_l1: EthAddressLike, nonce: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
-    /** claim_private(secret_hash_for_redeeming_minted_notes: field, amount: field, secret_for_L1_to_L2_message_consumption: field) */
-    claim_private: ((secret_hash_for_redeeming_minted_notes: FieldLike, amount: FieldLike, secret_for_L1_to_L2_message_consumption: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
-
-    /** constructor(token: struct, portal_address: struct) */
-    constructor: ((token: AztecAddressLike, portal_address: EthAddressLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+    /** get_portal_address_public() */
+    get_portal_address_public: (() => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
     /** exit_to_l1_private(token: struct, recipient: struct, amount: field, caller_on_l1: struct, nonce: field) */
     exit_to_l1_private: ((token: AztecAddressLike, recipient: EthAddressLike, amount: FieldLike, caller_on_l1: EthAddressLike, nonce: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
-    /** compute_note_hash_and_nullifier(contract_address: struct, nonce: field, storage_slot: field, note_type_id: field, serialized_note: array) */
-    compute_note_hash_and_nullifier: ((contract_address: AztecAddressLike, nonce: FieldLike, storage_slot: FieldLike, note_type_id: FieldLike, serialized_note: FieldLike[]) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
-
-    /** get_portal_address_public() */
-    get_portal_address_public: (() => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
-
-    /** claim_public(to: struct, amount: field, secret: field, message_leaf_index: field) */
-    claim_public: ((to: AztecAddressLike, amount: FieldLike, secret: FieldLike, message_leaf_index: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+    /** claim_private(secret_hash_for_redeeming_minted_notes: field, amount: field, secret_for_L1_to_L2_message_consumption: field) */
+    claim_private: ((secret_hash_for_redeeming_minted_notes: FieldLike, amount: FieldLike, secret_for_L1_to_L2_message_consumption: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
 
     /** get_token() */
     get_token: (() => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+
+    /** get_portal_address() */
+    get_portal_address: (() => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+
+    /** constructor(token: struct, portal_address: struct) */
+    constructor: ((token: AztecAddressLike, portal_address: EthAddressLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
+
+    /** claim_public(to: struct, amount: field, secret: field, message_leaf_index: field) */
+    claim_public: ((to: AztecAddressLike, amount: FieldLike, secret: FieldLike, message_leaf_index: FieldLike) => ContractFunctionInteraction) & Pick<ContractMethod, 'selector'>;
   };
 }
